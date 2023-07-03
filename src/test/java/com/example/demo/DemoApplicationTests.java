65bd0cbe49eae26a40b600cfeee8a15a03c01a26
charles du <duyinhao@gmail.com>
2023-07-03 22:56:05 +0800

    Add in test for context loading of the reminder controller

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import com.example.demo.controller.ReminderController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureGraphQlTester;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.web.servlet.MockMvc;
+import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private ReminderController reminderController;
+
 	@Test
 	void contextLoads() {
+		assertThat(reminderController).isNotNull();
 	}
 
 }
