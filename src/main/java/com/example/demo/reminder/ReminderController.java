87ee6014dc06a070bb641686881937546b3b8407
charles du <duyinhao@gmail.com>
2023-07-04 14:30:13 +0800

    Implement setup for reminderControllerTest

--- src/main/java/com/example/demo/reminder/ReminderController.java
+++ src/main/java/com/example/demo/reminder/ReminderController.java
@@ -1,6 +1,6 @@
-package com.example.demo.controller;
+package com.example.demo.reminder;
 
-import com.example.demo.model.Reminder;
+import com.example.demo.reminder.Reminder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/Reminder")
 public class ReminderController {
+    ReminderService reminderService;
+    public ReminderController(ReminderService reminderService){
+        this.reminderService = reminderService;
+    }
     @GetMapping
     public Reminder getReminder(){
-        return null;
+        return reminderService.getLatestReminder();
     }
 
 
--- src/test/java/com/example/demo/reminder/ReminderControllerTest.java
+++ src/test/java/com/example/demo/reminder/ReminderControllerTest.java
@@ -1,5 +1,21 @@
 package com.example.demo.reminder;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+
+import java.util.Date;
+
+import static org.mockito.Mockito.when;
+
 public class ReminderControllerTest {
+    @Mock
+    ReminderService reminderService;
+
+    ReminderController reminderController;
+
+    @BeforeEach
+    public void setup(){
+        reminderController = new ReminderController(reminderService);
+    }
 
 }
