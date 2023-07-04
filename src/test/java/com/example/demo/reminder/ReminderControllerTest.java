0d1becc28174ffe082142a2924878f6991013b2a
charles du <duyinhao@gmail.com>
2023-07-04 14:43:54 +0800

    Finish controller level test

--- src/test/java/com/example/demo/reminder/ReminderControllerTest.java
+++ src/test/java/com/example/demo/reminder/ReminderControllerTest.java
@@ -1,10 +1,13 @@
 package com.example.demo.reminder;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.util.Date;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 public class ReminderControllerTest {
@@ -15,7 +18,27 @@ public class ReminderControllerTest {
 
     @BeforeEach
     public void setup(){
+        MockitoAnnotations.initMocks(this);
         reminderController = new ReminderController(reminderService);
+    }
+    @Test
+    public void getReminder(){
+        Reminder reminderMock = new Reminder();
+        Date date = new Date();
+        long dateInTime = 100;
+        date.setTime(dateInTime);
+
+        String eventName = "Test Event";
+
+        reminderMock.setDate(date);
+        reminderMock.setName(eventName);
+
+        when(reminderService.getLatestReminder()).thenReturn(reminderMock);
+
+        Reminder reminder = reminderService.getLatestReminder();
+
+        assertThat(reminder.getDate().getTime()).isEqualTo(dateInTime);
+        assertThat(reminder.getName()).isEqualTo(eventName);
     }
 
 }
